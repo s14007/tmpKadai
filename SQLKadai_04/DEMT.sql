484bb22e8548d157ef357eeb2ce9ab4d32758352
s14007 <s14007@std.it-college.ac.jp>
2017-05-02 16:46:03 +0900

    firstCommit

---
+++ SQLKadai_04/DEMT.sql
@@ -0,0 +1,9 @@
+CREATE TABLE DEMT (
+  DEPTNO INT,
+  NAME VARCHAR(14),
+  LOC VARCHAR(13)
+);
+insert into DEMT value (10,'ACCOUNTING','NEW YORK');
+insert into DEMT value (20,'RESEARCH','DALLAS');
+insert into DEMT value (30,'SALES','CHICAGO');
+insert into DEMT value (40,'OPERATIONS','BOSTON');
